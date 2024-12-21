42319b0cad6893290c9ec404e39a8a8c70dd3ab9
Jahid Hasan <mdminhajulislamshobuj@gmail.com>
2024-12-21 14:22:58 +0600

    update

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -5,7 +5,6 @@ dotenv.config({ path: path.join(process.cwd(), '.env') })
 export default {
   port: process.env.PORT,
   url: process.env.URL,
-  defaultPassword: process.env.DEFAULT_PASS,
   bcrypt_salt_round: process.env.BCRYPT_SALT_ROUNDS,
   jwt_access_secret: process.env.JWT_ACCESS_SECRET,
 }
